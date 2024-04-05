f3ceb89c5be26f82ca41827f1df02316ca512e89
FemiBlack <tadefemi2@hotmail.com>
2024-04-05 16:21:57 +0100

    feat: add subscription plans page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'off-black': '#0e0e0e',
+      },
       screens: {
         xs: '360px', // Define your custom 'xs' breakpoint
       },

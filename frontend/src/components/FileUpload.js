5fcb310542d232d92618142e8ec61707cbdaf2aa
nomadqwerty <davexmike72@gmail.com>
2024-05-20 16:06:10 +0100

    voice recorder

--- frontend/src/components/FileUpload.js
+++ frontend/src/components/FileUpload.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDropzone } from 'react-dropzone';
 
 const FileUpload = ({
@@ -8,6 +8,11 @@ const FileUpload = ({
   setUploadedFiles,
 }) => {
   const [error, setError] = useState(false);
+  let [mediaChunks, setMediaChunks] = useState([]);
+  let [recorder, setRecorder] = useState(null);
+  let [stream, setStream] = useState(null);
+  let [action, setAction] = useState('start');
+  let chunksStream = [];
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     accept: acceptExtensions,
@@ -27,6 +32,8 @@ const FileUpload = ({
           )
         );
 
+        console.log(validFiles);
+
         setUploadedFiles([...uploadedFiles, ...validFiles]);
         setError(false);
       } else {
@@ -35,8 +42,65 @@ const FileUpload = ({
     },
   });
 
+  const onVoiceRecord = async () => {
+    try {
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        if (!recorder) {
+          let audioStream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+          });
+          setRecorder(new MediaRecorder(audioStream));
+          setStream(audioStream);
+        } else {
+          recorder.stop();
+          setAction('start');
+        }
+      }
+    } catch (error) {}
+  };
+  const onRemoveRecord = async () => {
+    try {
+      if (stream) {
+        stream.getTracks().forEach(function (track) {
+          track.stop();
+        });
+        const playBack = document.querySelector('#audioTracks');
+        playBack.src = null;
+        setRecorder(null);
+      }
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    if (recorder) {
+      recorder.ondataavailable = (e) => {
+        chunksStream.push(e.data);
+      };
+
+      recorder.onstop = (e) => {
+        setMediaChunks(chunksStream);
+        chunksStream = [];
+      };
+      recorder.start();
+      setAction('stop');
+    }
+  }, [recorder]);
+  useEffect(() => {
+    if (mediaChunks.length > 0) {
+      const playBack = document.querySelector('#audioTracks');
+      const blob = new Blob(mediaChunks, { type: 'audio/ogg; codecs=opus' });
+      setMediaChunks([]);
+      console.log(blob);
+
+      const audioUrl = window.URL.createObjectURL(blob);
+      const newFile = new File([blob], 'recordingAttachment');
+      setUploadedFiles([...uploadedFiles, newFile]);
+      playBack.src = audioUrl;
+    }
+  }, [mediaChunks]);
+
   return (
-    <>
+    <div style={{ width: '50vw' }}>
       <div
         {...getRootProps()}
         className={`fileUpload ${isDragActive ? 'active' : ''}`}
@@ -53,7 +117,52 @@ const FileUpload = ({
           Nur PDF-Dateien mit einer Größe von bis zu 1.5 MB sind erlaubt.
         </p>
       )}
-    </>
+
+      <div
+        className="voiceRecordWrap"
+        style={{ display: 'flex', paddingTop: '10px', paddingBottom: '10px' }}
+      >
+        <button
+          className="voiceRecordBtn"
+          id="mic"
+          style={{
+            background: '#eee9e9',
+            marginRight: '15px',
+            marginLeft: '5px',
+            border: '1px solid red',
+            borderRadius: '25%',
+            width: '50px',
+            cursor: 'pointer',
+          }}
+          onClick={(e) => {
+            e.preventDefault();
+            onVoiceRecord();
+          }}
+        >
+          <span>{action}</span>
+        </button>
+        <button
+          className="voiceRecordBtn"
+          id="delete"
+          onClick={(e) => {
+            e.preventDefault();
+            onRemoveRecord();
+          }}
+          style={{
+            background: '#eee9e9',
+            marginRight: '15px',
+            marginLeft: '5px',
+            border: '1px solid red',
+            borderRadius: '25%',
+            width: '50px',
+            cursor: 'pointer',
+          }}
+        >
+          <span>delete</span>
+        </button>
+        <audio controls id="audioTracks"></audio>
+      </div>
+    </div>
   );
 };
 

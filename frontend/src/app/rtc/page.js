f1626e0218383f7453a40ae2312f1a089154ffc3
Dave3U <davidujile@icloud.com>
2024-05-23 18:28:38 +0100

    added suspense boundary import as a useSearchParams dependency

--- frontend/src/app/rtc/page.js
+++ frontend/src/app/rtc/page.js
@@ -1,8 +1,12 @@
 import { FullRtc } from "@/Components/FullRTC/FullRtc";
+import { Suspense } from "react";
 import Lobby from "@/Components/FullRTC/Lobby";
 import Link from "next/link";
 
 export default function Rtc() {
-  return (<FullRtc></FullRtc>);
-  // return (<Lobby></Lobby>)
+  return (
+    <Suspense>
+      <FullRtc></FullRtc>
+    </Suspense>
+  );
 }

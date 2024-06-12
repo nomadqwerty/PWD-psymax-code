c63fd8cc8baeca36e6d4d15bdb6cde902aada5a7
FemiBlack <tadefemi2@hotmail.com>
2024-06-12 14:16:47 +0100

    chore: setup docker services
    - Setup frontend, backend, mongodb and redis services

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,25 @@
 module.exports = {
-  output: "standalone",
+  output: 'standalone',
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination:
+          process.env.NODE_ENV === 'development'
+            ? 'http://localhost:4000/api/:path*'
+            : 'http://backend:4000/api/:path*',
+      },
+    ];
+  },
   env: {
-    NEXT_PUBLIC_PAGINATION_LIMIT: "10",
-    NEXT_PUBLIC_API_HOST: "http://localhost:4000/api",
-    NEXT_PUBLIC_LOGOUT_TIMER: "10",
+    NEXT_PUBLIC_PAGINATION_LIMIT: '10',
+    // NEXT_PUBLIC_API_HOST: "http://localhost:4000/api",
+    NEXT_PUBLIC_API_HOST: '/api',
+    NEXT_PUBLIC_LOGOUT_TIMER: '10',
   },
 };

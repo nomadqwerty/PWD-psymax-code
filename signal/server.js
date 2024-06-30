710bbb56dd2dfa674d5dd50a93a6d31266fe7506
nomadqwerty <davexmike72@gmail.com>
2024-06-30 21:26:09 +0100

    test

--- signal/server.js
+++ signal/server.js
@@ -215,6 +215,141 @@ io.on('connection', async (socket) => {
   //     //   socket.on('chat message', async (msg, clientOffset, callback) => {
   //     console.log('message: ' + data);
   //   });
+  console.log('incoming connection');
+  const userName = socket.handshake.auth.userName;
+  const accessKey = socket.handshake.auth.accessKey;
+
+  connectedSockets.push({
+    socketId: socket.id,
+    userName,
+    accessKey,
+  });
+
+  socket.on('newOfferScreen', (data) => {
+    let newOffer = data.offer;
+    console.log(data.id, 'data');
+
+    console.log(userName, accessKey);
+    console.log('received new offer');
+    offersScreen.push({
+      offererUserName: userName,
+      offerAccessKey: accessKey,
+      offer: newOffer,
+      offerIceCandidates: [],
+      answererUserName: null,
+      answer: null,
+      answererIceCandidates: [],
+      callerId: data.id,
+    });
+    //send out to all connected sockets EXCEPT the caller
+    socket.broadcast.emit('newOfferAwaitingScreen', offersScreen.slice(-1));
+  });
+
+  socket.on('newAnswerScreen', (data, ackFunction) => {
+    let offerObj = data.offer;
+    let answerId = data.answerId;
+    let accessKey = data.answererAccessKey;
+    const socketToAnswer = connectedSockets.find(
+      (s) =>
+        s.userName === offerObj.offererUserName &&
+        s.accessKey === offerObj.offerAccessKey
+    );
+
+    if (!socketToAnswer) {
+      console.log('No matching socket');
+      return;
+    }
+    //we found the matching socket, so we can emit to it!
+    const socketIdToAnswer = offerObj.callerId;
+    console.log(socketIdToAnswer);
+    //we find the offer to update so we can emit it
+    const offerToUpdate = offersScreen.find(
+      (o) =>
+        o.offererUserName === offerObj.offererUserName &&
+        o.offerAccessKey === offerObj.offerAccessKey
+    );
+
+    if (!offerToUpdate) {
+      console.log('No OfferToUpdate');
+      return;
+    }
+    //send back to the answerer all the iceCandidates we have already collected
+    ackFunction(offerToUpdate.offerIceCandidates);
+    offerToUpdate.answer = offerObj.answer;
+    offerToUpdate.answererUserName = userName;
+    offerToUpdate.answerId = answerId;
+    offerToUpdate.answererAccessKey = accessKey;
+    //socket has a .to() which allows emiting to a "room"
+    //every socket has it's own room
+    console.log('answer emit');
+    socket.to(socketIdToAnswer).emit('answerResponseScreen', offerToUpdate);
+  });
+
+  socket.on('sendIceCandidateToSignalingServerScreen', (iceCandidateObj) => {
+    const { didIOffer, iceUserName, iceCandidate, iceAccessKey } =
+      iceCandidateObj;
+    // console.log(iceCandidate);
+    if (didIOffer) {
+      //this ice is coming from the offerer. Send to the answerer
+      const offerInOffers = offersScreen.find(
+        (o) => o.offerAccessKey === iceAccessKey
+      );
+      console.log(offerInOffers.answerId, 'here');
+      if (offerInOffers) {
+        offerInOffers.offerIceCandidates.push(iceCandidate);
+        // 1. When the answerer answers, all existing ice candidates are sent
+        // 2. Any candidates that come in after the offer has been answered, will be passed through
+        if (offerInOffers.answererAccessKey && offerInOffers.answerId) {
+          //pass it through to the other socket
+          const socketToSendTo = connectedSockets.find(
+            (s) => s.accessKey === offerInOffers.answererAccessKey
+          );
+          if (socketToSendTo) {
+            console.log(offerInOffers.answerId, 'answer id');
+            socket
+              .to(offerInOffers.answerId)
+              .emit('receivedIceCandidateFromServerScreen', iceCandidate);
+          } else {
+            console.log('Ice candidate recieved but could not find answere');
+          }
+        }
+      }
+    } else {
+      //this ice is coming from the answerer. Send to the offerer
+      //pass it through to the other socket
+      const offerInOffers = offersScreen.find(
+        (o) => o.offerAccessKey === iceAccessKey
+      );
+      const socketToSendTo = connectedSockets.find(
+        (s) => s.accessKey === offerInOffers.answererAccessKey
+      );
+      if (socketToSendTo && offerInOffers.callerId) {
+        console.log(offerInOffers.callerId, 'call id');
+        socket
+          .to(offerInOffers.callerId)
+          .emit('receivedIceCandidateFromServerScreen', iceCandidate);
+      } else {
+        console.log('Ice candidate recieved but could not find offerer');
+      }
+    }
+    // console.log(offersScreen)
+  });
+
+  socket.on('addedCallerIceScreen', (data) => {
+    if (data.myIce) {
+      console.log('received caller ice');
+      socket.to(data.fromId).emit('receivedCallerIceScreen', data);
+      console.log(data.fromId);
+      console.log('sent');
+    } else {
+      console.log('added caller ice');
+      socket.to(data.toId).emit('hasCallerIceScreen', data);
+    }
+  });
+  socket.on('newMessage', (data) => {
+    console.log(data, 'new message');
+    socket.to(data.to).emit('incomingMessage', data);
+  });
 });
 
 // io.compress(true);
